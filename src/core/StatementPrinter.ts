ea91267f3da281179d7929382b18d1c511d33f7a
Miguel Angel Gomez Alvarez <miguelghz@Miguels-MacBook-Air.local>
2022-07-26 13:11:49 +0100

    refactor

--- src/core/StatementPrinter.ts
+++ src/core/StatementPrinter.ts
@@ -8,14 +8,21 @@ export class StatementPrinter {
 
 	print(transactions: Transaction[]) {
 		this.console.log(this.header);
-		let runningBalance = 0;
-		transactions
-			.map((transaction) => {
-				runningBalance += transaction.amount;
-				return this.formatStatementLine(transaction, runningBalance);
-			})
+		this.printStatements(transactions);
+	}
+
+	private printStatements(transactions: Transaction[]) {
+		this.generateStatementLine(transactions)
 			.reverse()
 			.forEach((line) => this.console.log(line));
+	}
+
+	private generateStatementLine(transactions: Transaction[]) {
+		let runningBalance = 0;
+		return transactions.map((transaction) => {
+			runningBalance += transaction.amount;
+			return this.formatStatementLine(transaction, runningBalance);
+		});
 	}
 
 	private formatStatementLine(transaction: Transaction, runningBalance: number) {
